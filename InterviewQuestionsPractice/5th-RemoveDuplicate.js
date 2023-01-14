e65891cbcda2d9548a481c73c602530268ad4773
Rajesh Saini <saini222302@gmail.com>
2023-01-14 16:14:34 +0530

    JS interview Questiuon

--- InterviewQuestionsPractice/5th-RemoveDuplicate.js
+++ InterviewQuestionsPractice/5th-RemoveDuplicate.js
@@ -1,5 +1,5 @@
 //Remove Duplicate
-let arr = [1,2,3,2,1,4,5,8,5,7]
+let arr = [1,2,3,2,1,4,5,8,5]
 function removeDuplicates(arr) {
     return arr.filter((item, 
         index) => arr.indexOf(item) === index);
