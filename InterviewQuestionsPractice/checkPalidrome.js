f56116f9ec3cd05597833025ef38a709cd91f572
Rajesh Saini <saini222302@gmail.com>
2023-01-19 08:39:21 +0530

    interview DSA Question

---
+++ InterviewQuestionsPractice/checkPalidrome.js
@@ -0,0 +1,32 @@
+// //check palindrome  in javascript without using in build method 
+// function checkPalindrome (n){
+//     let len = n.lenght-1;
+//     for(i=0; i<len/2; i++){
+//         let f = n[i]
+//         let b = len-i
+//         if(!f == b){
+//             return true
+//         }else{
+//             return false
+//         }
+//     }
+// }
+// console.log(checkPalindrome("arora"))
+  function check_palindrome( str )
+    {
+      let j = str.length -1;
+      for( let i = 0 ; i < j/2 ;i++)
+      {
+        let x = str[i] ;//forward character 
+        let y = str[j-i];//backward character
+        if( x != y)
+        {
+          // return false if string not match
+          return false;
+        }
+      }
+      /// return true if string is palindrome
+      return true;
+       
+    }
+    console.log(check_palindrome(121))
